916d4e3d4ea69548a7ff101c532e5289923c553a
chohyunwoo <wizardchw@naver.com>
2023-08-07 23:09:10 +0900

    수 이어쓰기 추가

---
+++ Cpp/Implementation/1515_수_이어_쓰기/main.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+#define DIGIT_LIMIT 5
+string str;
+int copiedNumber, currentNumber = 1, digitSize = 1, digits[DIGIT_LIMIT], digitIdx;
+
+int main(void)
+{
+
+    cin >> str;
+
+    int strIdx = 0, strLen = str.size(), divid;
+    while (strIdx < strLen)
+    {
+        copiedNumber = currentNumber;
+        char ch = str.at(strIdx);
+
+        divid = pow(10, DIGIT_LIMIT - 1);
+        digitSize = -1;
+
+        // 각 자릿수 설정
+        for (int i = 0; i < DIGIT_LIMIT; i++)
+        {
+            digits[i] = copiedNumber / (divid); // 10^(DIGIT_LIMIT-1)의 자리 ~ 1의 자리까지 구하기
+            if (digitSize < 0 && digits[i] > 0) // 가장 먼저 0보다 큰 수가 나온 자릿수가 그 수의 자릿수
+                digitSize = DIGIT_LIMIT - i;
+
+            copiedNumber -= digits[i] * divid; // 다음 자릿수를 구하기 위한 설정
+            divid /= 10;
+        }
+
+        // 구한 숫자와 일치하는 문자 개수만큼 건너뛰기
+        for (int i = DIGIT_LIMIT - digitSize; i < DIGIT_LIMIT; i++)
+        {
+            if (ch == digits[i] + '0')
+            {
+                strIdx++;
+                if (strIdx == strLen)
+                    break;
+                ch = str.at(strIdx);
+            }
+        }
+
+        currentNumber++;
+    }
+
+    cout << currentNumber - 1;
+}
